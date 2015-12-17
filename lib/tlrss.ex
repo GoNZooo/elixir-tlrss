e29fe7e6627beb95c2b729f4896137f484f699a1
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-17 17:20:14 +0200

    Add parsing of RSS feed

--- lib/tlrss.ex
+++ lib/tlrss.ex
@@ -1,2 +1,15 @@
 defmodule TLRSS do
+  def rss_data do
+    rss_url = Application.get_env(:tlrss, :rss_url)
+    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(rss_url)
+    {:ok, feed, _} = FeederEx.parse body 
+    feed
+  end
+
+  def rss_entries do
+    rss_url = Application.get_env(:tlrss, :rss_url)
+    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(rss_url)
+    {:ok, feed, _} = FeederEx.parse body 
+    feed.entries
+  end
 end
