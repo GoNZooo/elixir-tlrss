41db921ba4df7459caa09bbf3bd30cd6a20f772f
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-18 15:18:59 +0200

    Remove db stuff and replace with Item struct

--- lib/tlrss/item.ex
+++ lib/tlrss/item.ex
@@ -1,29 +1,3 @@
 defmodule TLRSS.Item do
-  use Ecto.Schema
-  import Ecto.Changeset
-
-  schema "items" do
-    field(:tlid, :string)
-    field(:name, :string)
-    field(:link, :string)
-    field(:downloaded, :boolean, default: false)
-  end
-
-  @required_fields ~w(tlid name link)
-  @optional_fields ~w(downloaded)
-
-  @doc """
-  Creates a changeset based on the `model` and `params`.
-
-  If no params are provided, an invalid changeset is returned
-  with no validation performed.
-  """
-  def changeset(model, params \\ :empty) do
-    model
-    |> cast(params, @required_fields, @optional_fields)
-    |> unique_constraint(:id)
-    |> validate_length(:tlid, min: 1)
-    |> validate_length(:name, min: 1)
-    |> validate_length(:link, min: 1)
-  end
+  defstruct :tlid, :name, :link, :downloaded
 end
--- lib/tlrss/rss.ex
+++ lib/tlrss/rss.ex
@@ -14,24 +14,4 @@ defmodule TLRSS.RSS do
     {:ok, feed, _} = FeederEx.parse body 
     feed.entries
   end
-
-  defp add_entry(e) do
-    changeset = TLRSS.Item.changeset %TLRSS.Item{}, %{tlid: e.id,
-                                                      name: e.title,
-                                                      link: e.link}
-    TLRSS.Repo.insert changeset
-  end
-
-  def log_entries(entries) do
-    Enum.map entries,
-    fn e ->
-      case add_entry(e) do
-        {:ok, _} ->
-          Logger.info "Added #{e.name} [#{e.id}]"
-        {:error, changeset} ->
-          Logger.info "Could not add #{e.name}"
-        Enum.each changeset.errors, fn {f, e} -> Logger.info "#{f}: #{e}" end
-      end
-    end
-  end
 end
