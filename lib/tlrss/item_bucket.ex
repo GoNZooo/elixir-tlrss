ab82c75f67e2ccba2b9972856a3a262c8a86b1d5
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-22 00:23:49 +0200

    Add `remove_item` and init func

--- lib/tlrss/item_bucket.ex
+++ lib/tlrss/item_bucket.ex
@@ -1,20 +1,24 @@
 defmodule TLRSS.ItemBucket do
   use GenServer
 
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, opts)
-  end
-
-  def add_item(pid, item) do
-    GenServer.call(pid, {:add_item, item})
+  def start_link(init_items, opts \\ []) do
+    GenServer.start_link(__MODULE__, init_items, opts)
   end
 
   def get_items(pid) do
     GenServer.call(pid, :get_items)
   end
 
+  def add_item(pid, item) do
+    GenServer.call(pid, {:add_item, item})
+  end
+
   def add_items(pid, items) do
     GenServer.call(pid, {:add_items, items})
+  end
+
+  def remove_item(pid, item) do
+    GenServer.cast(pid, {:remove_item, item})
   end
 
   def seen_item?(pid, item) do
@@ -25,16 +29,20 @@ defmodule TLRSS.ItemBucket do
     Enum.member? items, item
   end
 
+  def init(init_items) do
+    {:ok, init_items}
+  end
+
+  def handle_call(:get_items, _from, items) do
+    {:reply, {:items, items}, items}
+  end
+
   def handle_call({:add_item, item}, _from, items) do
     if item_seen? items, item do
       {:reply, {:seen, item}, items}
     else
       {:reply, {:ok, item}, [item|items]}
     end
-  end
-
-  def handle_call(:get_items, _from, items) do
-    {:reply, {:items, items}, items}
   end
 
   def handle_call({:add_items, new_items}, _from, items) do
@@ -50,5 +58,9 @@ defmodule TLRSS.ItemBucket do
     else
       {:reply, {:not_seen, item}, items}
     end
+  end
+
+  def handle_cast({:remove_item, item}, items) do
+    {:noreply, List.delete(items, item)}
   end
 end
