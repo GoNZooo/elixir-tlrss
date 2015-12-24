a588f6a3bef5431f989c9d69e5f23244b0034577
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-24 10:18:56 +0200

    Add test for all items already present

--- test/tlrss_test.exs
+++ test/tlrss_test.exs
@@ -2,78 +2,91 @@ defmodule TLRSSTest do
   use ExUnit.Case
   doctest TLRSS
 
+  alias TLRSS.ItemBucket
+
   test "add items" do
-    {:ok, pid} = TLRSS.ItemBucket.start_link
+    {:ok, pid} = ItemBucket.start_link
     items = [42,1337,13,5,23]
 
-    TLRSS.ItemBucket.add_items pid, items
-    {:items, is} = TLRSS.ItemBucket.get_items pid
+    ItemBucket.add_items pid, items
+    {:items, is} = ItemBucket.get_items pid
 
     assert is == items
   end
 
   test "seen_item? after init" do
     item = %TLRSS.Item{tlid: "tlid", name: "name", link: "link"}
-    {:ok, pid} = TLRSS.ItemBucket.start_link([item])
+    {:ok, pid} = ItemBucket.start_link([item])
 
-    {:seen, i} = TLRSS.ItemBucket.seen_item? pid, item
+    {:seen, i} = ItemBucket.seen_item? pid, item
     assert i == item
   end
 
   test "seen_item? after adding item" do
-    {:ok, pid} = TLRSS.ItemBucket.start_link
+    {:ok, pid} = ItemBucket.start_link
     item = %TLRSS.Item{tlid: "tlid", name: "name", link: "link"}
 
-    TLRSS.ItemBucket.add_item pid, item
-    {:seen, i} = TLRSS.ItemBucket.seen_item? pid, item
+    ItemBucket.add_item pid, item
+    {:seen, i} = ItemBucket.seen_item? pid, item
     assert i == item
   end
 
   test "remove item" do
-    {:ok, pid} = TLRSS.ItemBucket.start_link([1,2,3,4,5])
+    {:ok, pid} = ItemBucket.start_link([1,2,3,4,5])
 
-    TLRSS.ItemBucket.remove_item pid, 2
-    TLRSS.ItemBucket.remove_item pid, 4
+    ItemBucket.remove_item pid, 2
+    ItemBucket.remove_item pid, 4
 
-    {:items, items} = TLRSS.ItemBucket.get_items pid
+    {:items, items} = ItemBucket.get_items pid
     assert items == [1,3,5]
   end
 
   test "remove items" do
-    {:ok, pid} = TLRSS.ItemBucket.start_link([1,2,3,4,5])
+    {:ok, pid} = ItemBucket.start_link([1,2,3,4,5])
 
-    TLRSS.ItemBucket.remove_items(pid, [2,4])
+    ItemBucket.remove_items(pid, [2,4])
 
-    {:items, items} = TLRSS.ItemBucket.get_items pid
+    {:items, items} = ItemBucket.get_items pid
     assert items == [1,3,5]
   end
 
   test "remove matching" do
-    {:ok, pid} = TLRSS.ItemBucket.start_link([1,2,3,4,5])
+    {:ok, pid} = ItemBucket.start_link([1,2,3,4,5])
 
-    TLRSS.ItemBucket.remove_matching(pid, fn n -> rem(n, 2) == 0 end)
+    ItemBucket.remove_matching(pid, fn n -> rem(n, 2) == 0 end)
 
-    {:items, items} = TLRSS.ItemBucket.get_items pid
+    {:items, items} = ItemBucket.get_items pid
     assert items == [1,3,5]
   end
 
   test "add items, some already present" do
-    {:ok, pid} = TLRSS.ItemBucket.start_link([1,2,3,4,5])
+    {:ok, pid} = ItemBucket.start_link([1,2,3,4,5])
 
     {{:ok, new_items},
-     {:seen, seen_items}} = TLRSS.ItemBucket.add_items pid, [2,3, 42, 1337]
+     {:seen, seen_items}} = ItemBucket.add_items pid, [2,3, 42, 1337]
 
     assert new_items == [42, 1337]
     assert seen_items == [2,3]
   end
 
-  test "add items, all present" do
-    {:ok, pid} = TLRSS.ItemBucket.start_link([1,2,3,4,5])
+  test "add items, all already present" do
+    {:ok, pid} = ItemBucket.start_link([1,2,3,4,5])
 
     {{:ok, new_items},
-     {:seen, seen_items}} = TLRSS.ItemBucket.add_items pid, [2,3, 1, 4, 5]
+     {:seen, seen_items}} = ItemBucket.add_items pid, [2,3, 1, 4, 5]
 
     assert new_items == []
     assert seen_items == [2, 3, 1, 4, 5]
+  end
+
+  test "add items, none already present" do
+    {:ok, pid} = ItemBucket.start_link([1,2,3,4])
+    items_to_add = [42, 1337, 5, 23]
+
+    {{:ok, new_items},
+     {:seen, seen_items}} = ItemBucket.add_items pid, items_to_add
+
+    assert new_items == [42, 1337, 5, 23]
+    assert seen_items == []
   end
 end
