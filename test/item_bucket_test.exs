805fbda7a548dd40f707a7f2f0ae0e440b489550
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-29 12:23:14 +0200

    Import ItemBucket in test module
    Natural consequence of moving tests into specific module
    for testing this module in particular

--- test/item_bucket_test.exs
+++ test/item_bucket_test.exs
@@ -2,59 +2,68 @@ defmodule ItemBucketTest do
   use ExUnit.Case
   doctest TLRSS.ItemBucket
 
-  alias TLRSS.ItemBucket
+  import TLRSS.ItemBucket,
+  only: [
+    start_link: 0,
+    start_link: 1,
+    add_items: 2,
+    get_items: 1,
+    remove_items: 2,
+    remove_matching: 2,
+
+  ]
 
   test "add items" do
-    {:ok, pid} = ItemBucket.start_link
+    {:ok, pid} = start_link
     items = [42, 1337, 13, 5, 23]
 
-    ItemBucket.add_items pid, items
-    {:items, is} = ItemBucket.get_items pid
+    add_items pid, items
+    {:items, is} = get_items pid
 
     assert is == items
   end
 
   test "remove items" do
-    {:ok, pid} = ItemBucket.start_link [1, 2, 3, 4, 5]
+    {:ok, pid} = start_link [1, 2, 3, 4, 5]
 
-    ItemBucket.remove_items pid, [2, 4] 
+    remove_items pid, [2, 4] 
 
-    {:items, items} = ItemBucket.get_items pid
+    {:items, items} = get_items pid
     assert items == [1, 3, 5]
   end
 
   test "remove matching" do
-    {:ok, pid} = ItemBucket.start_link [1, 2, 3, 4, 5]
+    {:ok, pid} = start_link [1, 2, 3, 4, 5]
 
-    ItemBucket.remove_matching pid, &(rem(&1, 2) == 0)
+    remove_matching pid, &(rem(&1, 2) == 0)
 
-    {:items, items} = ItemBucket.get_items pid
+    {:items, items} = get_items pid
     assert items == [1, 3, 5]
   end
 
   test "add items, some already present" do
-    {:ok, pid} = ItemBucket.start_link [1, 2, 3, 4, 5]
+    {:ok, pid} = start_link [1, 2, 3, 4, 5]
     is = [2, 3, 42, 1337]
 
-    {:ok, new_items} = ItemBucket.add_items pid, is
+    {:ok, new_items} = add_items pid, is
 
     assert new_items == [42, 1337]
   end
 
   test "add items, all already present" do
-    {:ok, pid} = ItemBucket.start_link [1, 2, 3, 4, 5]
+    {:ok, pid} = start_link [1, 2, 3, 4, 5]
     is = [2, 3, 1, 4, 5]
 
-    {:ok, new_items} = ItemBucket.add_items pid, is
+    {:ok, new_items} = add_items pid, is
 
     assert new_items == []
   end
 
   test "add items, none already present" do
-    {:ok, pid} = ItemBucket.start_link [1,2,3,4]
+    {:ok, pid} = start_link [1,2,3,4]
     is = [42, 1337, 5, 23]
 
-    {:ok, new_items} = ItemBucket.add_items pid, is
+    {:ok, new_items} = add_items pid, is
 
     assert new_items == [42, 1337, 5, 23]
   end
