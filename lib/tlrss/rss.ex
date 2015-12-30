8661b00840f2f06aba7a6a79ce9aff4245d07fa4
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-30 20:21:07 +0200

    Add func to convert RSS entry to %Item{}

--- lib/tlrss/rss.ex
+++ lib/tlrss/rss.ex
@@ -1,17 +1,19 @@
 defmodule TLRSS.RSS do
-  require Logger
+  alias TLRSS.Item
 
-  def data do
-    rss_url = Application.get_env(:tlrss, :rss_url)
+  def data(rss_url \\ Application.get_env(:tlrss, :rss_url)) do
     {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(rss_url)
     {:ok, feed, _} = FeederEx.parse body 
     feed
   end
 
-  def entries do
-    rss_url = Application.get_env(:tlrss, :rss_url)
+  def entries(rss_url \\ Application.get_env(:tlrss, :rss_url)) do
     {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(rss_url)
     {:ok, feed, _} = FeederEx.parse body 
     feed.entries
+  end
+
+  def rss_entry_to_item(%FeederEx.Entry{title: name, id: tlid, link: link}) do
+    %Item{name: name, tlid: tlid, link: link}
   end
 end
