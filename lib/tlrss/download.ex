077b8b3b2cfdb67dafe061f42321b53c4c1ff6d2
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-02 00:53:16 +0200

    Add module for downloads
    Uses :httpc (from erlang) to download files and write them to disk

---
+++ lib/tlrss/download.ex
@@ -0,0 +1,23 @@
+defmodule TLRSS.Download do
+  def download_file(url) when is_binary(url) do
+    filename = List.last String.split(url, "/")
+    {:ok, {{_, 200, 'OK'}, _headers, body}} = :httpc.request(:get, {String.to_char_list(url),
+                                                                    []},
+                                                             [],
+                                                             [body_format: :binary])
+    {{:filename, filename}, {:data, body}}
+  end
+
+  def write_file(filename, data) do
+    download_dir = Application.get_env :tlrss, :download_dir
+    full_path = download_dir <> filename
+
+    File.write! full_path, data
+  end
+
+  def start_download(url) do
+    dl = Task.Supervisor.async(TLRSS.DownloadSupervisor, fn -> download_file(url) end)
+    {{:filename, filename}, {:data, data}} = Task.await(dl)
+    write_file(filename, data)
+  end
+end
