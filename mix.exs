3a2585d00b51c1e854731940f7322cc4dd5ac8f0
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-03 18:46:28 +0200

    Add build dir (escript) and /bin to ignore

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule TLRSS.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: [main_module: TLRSS,
+               path: "bin/tlrss"],
      deps: deps]
   end
 
