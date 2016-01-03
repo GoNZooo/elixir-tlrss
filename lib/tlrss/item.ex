1307a161283b7ed272281eb0458e90adc63eec15
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-03 17:45:52 +0200

    Add inspect protocol implementation to %Item{}

--- lib/tlrss/item.ex
+++ lib/tlrss/item.ex
@@ -1,3 +1,18 @@
 defmodule TLRSS.Item do
   defstruct [:id, :name, :link]
+
+  defimpl Inspect, for: TLRSS.Item do
+    def inspect(%TLRSS.Item{id: id, name: name, link: link}, _) do
+      short_link = List.last(String.split(link, "/"))
+      short_id = List.last(String.split(id, "/"))
+
+      "#{name} [#{short_link}] (#{short_id})"
+    end
+  end
+
+  defimpl String.Chars, for: __MODULE__ do
+    def to_string(%TLRSS.Item{id: id, name: name, link: link}) do
+      "#{name} @ #{link} (#{id})"
+    end
+  end
 end
