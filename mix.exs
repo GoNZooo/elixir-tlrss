d9b089b43fb474253256fdee1430b1b219ac2a84
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-03 18:58:16 +0200

    Remove references to escript

--- mix.exs
+++ mix.exs
@@ -7,8 +7,6 @@ defmodule TLRSS.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     escript: [main_module: TLRSS,
-               path: "bin/tlrss"],
      deps: deps]
   end
 
