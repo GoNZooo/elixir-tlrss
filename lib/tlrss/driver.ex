ab2b6106d84780e1ce0003087b12e4862e4732b6
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-03 19:28:06 +0200

    Fix default sleep time, set it to 5 minutes

--- lib/tlrss/driver.ex
+++ lib/tlrss/driver.ex
@@ -6,12 +6,13 @@ defmodule TLRSS.Driver do
   alias TLRSS.ItemFilter
   alias TLRSS.Download
 
-  def start_link(sleep_time \\ 3000) do
+  def start_link(sleep_time \\ 300000) do
     Logger.info("Driver started.")
     Task.start_link(__MODULE__, :drive, [sleep_time])
   end
 
   def drive(sleep_time) do
+    Logger.debug("Driving...")
     {:entries, entries} = FeedReader.get_entries
     {:new_items, new_items} = ItemBucket.add_items entries
     {:matches, matches} = ItemFilter.matches? new_items
