d0f0a2fb9f358da73c232a6d323442190c7965ec
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-06 11:20:44 +0200

    Add functions for setting and adding filters

--- lib/tlrss/item_filter.ex
+++ lib/tlrss/item_filter.ex
@@ -18,6 +18,14 @@ defmodule TLRSS.ItemFilter do
     GenServer.call(pid, :get_filters)
   end
 
+  def set_filters(new_filters, pid \\ __MODULE__) do
+    GenServer.call(pid, {:set_filters, new_filters})
+  end
+
+  def add_filter(new_filter, pid \\ __MODULE__) do
+    GenServer.call(pid, {:add_filter, new_filter})
+  end
+
   ############
   # Internal #
   ############
@@ -33,6 +41,14 @@ defmodule TLRSS.ItemFilter do
   def handle_call({:matches?, items}, _from, filters) do
     matches = Enum.filter(items, &(any_matches?(&1, filters)))
     {:reply, {:matches, matches}, filters}
+  end
+
+  def handle_call({:set_filters, new_filters}, _from, _filters) do
+    {:reply, {:new_filters, new_filters}, new_filters}
+  end
+
+  def handle_call({:add_filter, new_filter}, _from, filters) do
+    {:reply, {:new_filter, new_filter}, [new_filter | filters]}
   end
 
   def handle_call(:get_filters, _from, filters) do
