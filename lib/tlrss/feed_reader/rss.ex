f88443e350effc93e811e9e24323e238c1932d91
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-10 00:06:17 +0200

    Add moduledoc and typespecs to RSS module

--- lib/tlrss/feed_reader/rss.ex
+++ lib/tlrss/feed_reader/rss.ex
@@ -1,6 +1,10 @@
 defmodule TLRSS.FeedReader.RSS do
+  @moduledoc"""
+  Handles the retrieval and minimal parsing of RSS feeds, using FeederEx.
+  """
   alias TLRSS.Item
 
+  @spec get_entries(String.t) :: [FeederEx.Entry.t]
   def get_entries(rss_url \\ Application.get_env(:tlrss, :rss_url)) do
     response = HTTPoison.get(rss_url, [{"Accept-Encoding:", "utf-8"}])
     {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(rss_url)
@@ -8,6 +12,7 @@ defmodule TLRSS.FeedReader.RSS do
     feed.entries
   end
 
+  @spec entry_to_item(FeederEx.Entry.t) :: Item.t
   def entry_to_item(%FeederEx.Entry{title: name, id: id, link: link}) do
     %Item{name: name, id: id, link: link}
   end
