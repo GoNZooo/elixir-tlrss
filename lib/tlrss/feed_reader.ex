5644d31f26e22647c23f500ddbfb1f04f38fbb46
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-09 11:01:27 +0200

    FeedReader now inits only with feed url

--- lib/tlrss/feed_reader.ex
+++ lib/tlrss/feed_reader.ex
@@ -1,11 +1,12 @@
 defmodule TLRSS.FeedReader do
   alias TLRSS.FeedReader.RSS
 
-  def start_link(init_feed, opts) do
+  def start_link(init_feed) do
     Task.start_link(__MODULE__, :read_rss, [init_feed])
   end
 
   def read_rss(init_feed, sleep_time \\ 300000) do
+    items_a = RSS.get_entries(init_feed)
     items = RSS.get_entries(init_feed) |> Enum.map(&RSS.entry_to_item/1)
     TLRSS.ItemBucket.add_items(items)
 
