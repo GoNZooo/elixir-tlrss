27cad670d544bdf1dd2663963f3777ea21a4e9a8
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-10 00:05:44 +0200

    Add moduledoc to FeedSpec
    Explaining rational for keeping it even when the feedspec is only one type.

--- lib/tlrss/feed_reader/feed_spec.ex
+++ lib/tlrss/feed_reader/feed_spec.ex
@@ -1,3 +1,8 @@
 defmodule TLRSS.FeedReader.FeedSpec do
+  @moduledoc"""
+  Module for specifying the structure of a FeedSpec. Kept here because it's
+  convenient to refer to the FeedSpec as FeedSpec.t in the program and only
+  changing in here when doing @specs.
+  """
   @type t :: String.t
 end
