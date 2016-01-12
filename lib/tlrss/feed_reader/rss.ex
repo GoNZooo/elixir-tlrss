3762b5548eb34c790d1839838c77087452db18a6
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-12 20:45:43 +0200

    Document functions in RSS module

--- lib/tlrss/feed_reader/rss.ex
+++ lib/tlrss/feed_reader/rss.ex
@@ -5,6 +5,7 @@ defmodule TLRSS.FeedReader.RSS do
   alias TLRSS.Item
 
   @spec get_entries(String.t) :: [FeederEx.Entry.t]
+  @doc"Fetches the specified RSS feed and returns all entries from it."
   def get_entries(rss_url \\ Application.get_env(:tlrss, :rss_url)) do
     response = HTTPoison.get(rss_url, [{"Accept-Encoding:", "utf-8"}])
     {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(rss_url)
@@ -13,6 +14,7 @@ defmodule TLRSS.FeedReader.RSS do
   end
 
   @spec entry_to_item(FeederEx.Entry.t) :: Item.t
+  @doc"Converts a %FeederEx.Entry{} to an %Item{}"
   def entry_to_item(%FeederEx.Entry{title: name, id: id, link: link}) do
     %Item{name: name, id: id, link: link}
   end
