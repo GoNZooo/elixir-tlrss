0d796b217bb64f41a82f3c24d3e2a3b299fef1dc
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-24 22:14:35 +0200

    Add note about FeedSpec.t

--- lib/tlrss/feed_reader/feed_spec.ex
+++ lib/tlrss/feed_reader/feed_spec.ex
@@ -3,6 +3,10 @@ defmodule TLRSS.FeedReader.FeedSpec do
   Module for specifying the structure of a FeedSpec. Kept here because it's
   convenient to refer to the FeedSpec as FeedSpec.t in the program and only
   changing in here when doing @specs.
+
+  This is to be used wherever a specification for which
+  feed to read is given, meaning it's for the startup
+  of FeedReaders and the like.
   """
   @type t :: String.t
 end
