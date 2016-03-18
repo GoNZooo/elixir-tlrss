bcac859cdfb9353c09e0fc5694e8ed6e2ebecb37
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-18 23:44:44 +0200

    Add FeedReader.Looper & Looper.Supervisor
    Handles task that will loop to spawn FeedReaders as well as handle data
    between the processes; flow.

---
+++ lib/tlrss/feed_reader/looper.ex
@@ -0,0 +1,26 @@
+defmodule TLRSS.FeedReader.Looper do
+  alias TLRSS.FeedReader.Supervisor, as: ReaderSup
+  #######
+  # API #
+  #######
+
+  def start_link(manager \\ TLRSS.FeedReader.Manager,
+                 sleep_time \\ 300_000,
+                 opts \\ [name: __MODULE__]) do
+    Task.start_link(__MODULE__, :read_loop, [manager, sleep_time])
+  end
+
+  ############
+  # Internal #
+  ############
+
+  def read_loop(manager, sleep_time) do
+    feeds = TLRSS.FeedReader.Manager.get_feeds(manager)
+
+    entries = feeds |> Enum.each(&(ReaderSup.start_child(&1)))
+    new_items = TLRSS.ItemBucket.add_items(entries)
+
+    :timer.sleep(sleep_time)
+    read_loop(manager, sleep_time)
+  end
+end
