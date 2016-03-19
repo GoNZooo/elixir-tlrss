0da51cb8fff6450a1f0ffaed3aeb6827d73ec1aa
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-19 04:32:13 +0200

    Flesh out Looper's processing

--- lib/tlrss/feed_reader/looper.ex
+++ lib/tlrss/feed_reader/looper.ex
@@ -20,11 +20,11 @@ defmodule TLRSS.FeedReader.Looper do
 
   def read_loop(manager, sleep_time) do
     feeds = TLRSS.FeedReader.Manager.get_feeds(manager)
-
-    entries = feeds |> Enum.each(&(ReaderSup.start_child(&1)))
+    entries = feeds
+    |> Enum.map(&(TLRSS.FeedReader.start_link(&1)))
+    |> Enum.flat_map(&(Task.await(&1)))
     new_items = TLRSS.ItemBucket.add_items(entries)
     matches = TLRSS.ItemFilter.filter(new_items)
-
     matches |> Enum.each(&(TLRSS.Download.download(&1)))
 
     :timer.sleep(sleep_time)
