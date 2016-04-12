80d658e4b4f41f2514bdf12b639bde74f05d4fc7
Rickard Andersson <gonz@severnatazvezda.com>
2016-04-12 23:28:02 +0300

    Clean up in RSS module

--- lib/tlrss/rss.ex
+++ lib/tlrss/rss.ex
@@ -35,16 +35,7 @@ defmodule TLRSS.RSS do
   end
 
   @spec get_entries(String.t) :: [FeederEx.Entry.t]
-  @doc"""
-  Fetches the specified RSS feed and returns all entries from it.
-
-  Will return {:error, reason} upon error and {:entries, [entry]} upon
-  success.
-
-  The error reason will be unpacked before returning it, meaning it can be
-  handled without consideration of %HTTPoison.Error{}.
-  """
-  defp get_entries(rss_url \\ Application.get_env(:tlrss, :rss_url)) do
+  defp get_entries(rss_url) do
     response = HTTPoison.get(rss_url, [{"Accept-Encoding:", "utf-8"}])
     case response do
       {:ok, %HTTPoison.Response{body: body}} ->
@@ -61,7 +52,6 @@ defmodule TLRSS.RSS do
   end
 
   @spec entry_to_item(FeederEx.Entry.t) :: Item.t
-  @doc"Converts a %FeederEx.Entry{} to an %Item{}"
   defp entry_to_item(%FeederEx.Entry{title: name, id: id, link: link}) do
     %Item{name: name, id: id, link: link}
   end
