6daad06ecf02a2a3d3f4cc7243087f84b04ec6ee
Rickard Andersson <gonz@severnatazvezda.com>
2016-04-12 21:18:36 +0300

    Slim down Looper, not actual process anymore

--- lib/tlrss/feed_reader/looper.ex
+++ lib/tlrss/feed_reader/looper.ex
@@ -1,45 +1,6 @@
 defmodule TLRSS.FeedReader.Looper do
-  @moduledoc"""
-  Drives the flow of the application. This was separated out of a chain of
-  forwarded results throughout the modules in order to keep them usable for
-  other uses. It's much like the Driver module that was originally a part of
-  the suite, but is instead a task that will run periodically.
-  """
-
-  require Logger
-
-  #######
-  # API #
-  #######
-
-  def start_link(manager \\ TLRSS.FeedReader.Manager,
-                 sleep_time \\ 300_000) do
-    Task.start_link(__MODULE__, :read_loop, [manager, sleep_time])
-  end
-
-  ############
-  # Internal #
-  ############
-
-  defp _handle_reads(reads) do
-    reads
-    |> Enum.reduce([],
-      fn task, all_items ->
-        case Task.yield(task) do
-          {:ok, items} ->
-            [items | all_items]
-          {:error, reason} ->
-            Logger.info("Error: #{reason}")
-            all_items
-        end
-      end)
-  end
-
-  def read_loop(manager, sleep_time) do
-    items = TLRSS.FeedReader.Manager.feeds(manager)
-    |> Enum.map(&TLRSS.FeedReader.start_reader(&1))
-    |> _handle_reads()
-    |> List.flatten()
+  def read_loop(feed, sleep_time) do
+    TLRSS.FeedReader.start_reader(feed)
     |> TLRSS.ItemBucket.add()
     |> TLRSS.ItemFilter.filter()
     |> Enum.each(&TLRSS.Download.download(&1))
